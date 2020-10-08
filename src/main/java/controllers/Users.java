6273d2313cf660d577f9639262c9755fc567fd6b
Mia Cavanagh <91751@farnborough.ac.uk>
2020-10-08 12:01:49 +0100

    I added my get/update/delete/save for my API requests to use with curl.

--- src/main/java/controllers/Users.java
+++ src/main/java/controllers/Users.java
@@ -21,7 +21,7 @@ public class Users{
         System.out.println("Invoked Users.UsersList()");
         JSONArray response = new JSONArray();
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, UserName FROM Users");
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, UserName FROM Users"); //maybe erorr
             ResultSet results = ps.executeQuery();
             while (results.next()==true) {
                 JSONObject row = new JSONObject();
