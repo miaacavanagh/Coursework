6aa79b8b8346acd1d79ad4cfe1732f3658f8c9d8
Mia Cavanagh <91751@farnborough.ac.uk>
2020-10-13 12:12:59 +0100

    added js file into js under client

---
+++ resources/client/js/Users.js
@@ -0,0 +1,26 @@
+"use strict";
+function getUsersList() {
+    debugger;
+    console.log("Invoked getUsersList()");     //console.log your BFF for debugging client side - also use debugger statement
+    const url = "/users/list/";    		// API method on web server will be in Users class, method list
+    fetch(url, {
+        method: "GET",				//Get method
+    }).then(response => {
+        return response.json();                 //return response as JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) { //checks if response from the web server has an "Error"
+            alert(JSON.stringify(response));    // if it does, convert JSON object to string and alert (pop up window)
+        } else {
+            formatUsersList(response);          //this function will create an HTML table of the data (as per previous lesson)
+        }
+    });
+}
+
+function formatUsersList(myJSONArray){
+    let dataHTML = "";
+    for (let item of myJSONArray) {
+        dataHTML += "<tr><td>" + item.UserID + "<td><td>" + item.UserName + "<tr><td>";
+    }
+    document.getElementById("UsersTable").innerHTML = dataHTML;
+}
+
